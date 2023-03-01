f303b066b28f9792aac22c2496a2157dd0be3d84
SuperPhantom <super.phantom.dev@gmail.com>
2023-03-01 10:43:55 -0800

    golang

--- dto/loginResponse.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type LoginResponse struct {
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token,omitempty"`
-}
--- dto/userResponse.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type UserResponse struct {
-	Id         string `json:"user_id"`
-	Username   string `json:"username"`
-	CustomerId string `json:"customer_id"`
-	Email      string `json:"email"`
-	Role       string `json:"role"`
-}
